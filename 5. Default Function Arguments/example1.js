258df8e78a1d803710a16e582c7cc42f10f3a361
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-04-19 19:21:43 -0300

    Added snippet for Default Function Arguments

---
+++ 5. Default Function Arguments/example1.js
@@ -0,0 +1,13 @@
+/*
+  function makeAjaxRequest(url, method) {
+    if (!method) {
+      method = GET;
+    }
+  }
+
+  makeAjaxRequest('google.com');
+  makeAjaxRequest('google.com', 'GET');
+*/
+function makeAjaxRequest(url, method = 'GET') {
+  return method;
+}
