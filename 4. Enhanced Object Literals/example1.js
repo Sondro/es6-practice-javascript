eb6b59c37fa0f2be5e7cb779d7b98544da709c78
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-04-19 18:46:45 -0300

    Added snippet for Enhanced Object Literals

---
+++ 4. Enhanced Object Literals/example1.js
@@ -0,0 +1,25 @@
+/*
+  In this case, we can omit 'inventory: inventory'
+  because they're absolutely the same.
+  We can also use foo() {} instead of foo: function()
+*/
+function createBookShop(inventory) {
+  return {
+    inventory,
+    inventoryValue() {
+      return this.inventory.reduce((total, book) => total + book.price, 0);
+    },
+    priceForTitle(title) {
+      return this.inventory.find(book => book.title === title).price;
+    }
+  }
+}
+
+const inventory = [
+  { title: 'Harry Potter', price: 10 },
+  { title: 'Eloquent Javascript', price: 15 }
+];
+
+const bookShop = createBookShop(inventory);
+bookShop.inventoryValue();
+bookShop.priceForTitle('Harry Potter');
