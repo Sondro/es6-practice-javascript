33755241a273c4177e1c926b6ebc130c03cf2a90
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-04-20 09:33:41 -0300

    Fixing syntax

--- 6. Rest and Spread/example1.js
+++ 6. Rest and Spread/example1.js
@@ -1,6 +1,6 @@
 // using Rest operator to capture an unknown number of arguments and putting them into a single array
 function addNumbers(...numbers) {
-  return numbers.reduce((sum, number) => return sum + number, 0);
+  return numbers.reduce((sum, number) => { return sum + number }, 0);
 }
 
 addNumbers(1, 2, 3, 4, 5);
