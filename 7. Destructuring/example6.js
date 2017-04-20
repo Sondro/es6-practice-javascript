2d9ac319f6130c583512486e1fa1de9d35e77801
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-04-20 12:37:38 -0300

    Added example6 of Destructuring

---
+++ 7. Destructuring/example6.js
@@ -0,0 +1,12 @@
+function signup({ username, password, email, dateOfBirth, city }) {
+  // create new user
+}
+const user = {
+  username: 'myname',
+  password: 'mypassword',
+  email: 'email@email.com',
+  dateOfBirth: '1/1/1000',
+  city: 'New York'
+}
+
+signup(user);
