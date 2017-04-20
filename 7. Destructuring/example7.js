e9d87849cc46ef0c7abe146f9d9b05739ec49b27
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2017-04-20 12:49:54 -0300

    Fixing example7

--- 7. Destructuring/example7.js
+++ 7. Destructuring/example7.js
@@ -3,3 +3,15 @@ const points = [
   [5, 1],
   [0, 3]
 ];
+/*
+  Destructuring the points array so it looks like:
+  [
+    { x: 4, y: 5 },
+    { x: 5, y: 1 },
+    { x: 0, y: 3 }
+  ]
+*/
+
+points.map(([ x, y ]) => {
+  return { x, y };
+});
